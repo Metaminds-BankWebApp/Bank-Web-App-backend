7d9fe21149ce1ac53fcb4a2d3127dbd87a1a4a7e
Buhuni Levanya <buhunilevanya@gmail.com>
2026-04-22 19:43:06 +0530

    branch-creation update

--- src/main/java/com/bank_web_app/backend/admin/repository/BranchRepository.java
+++ src/main/java/com/bank_web_app/backend/admin/repository/BranchRepository.java
@@ -1,12 +1,18 @@
 package com.bank_web_app.backend.admin.repository;
 
-import com.bank_web_app.backend.admin.entity.Branch;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.bank_web_app.backend.admin.entity.Branch;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
 
 	Optional<Branch> findByBranchCode(String branchCode);
 
 	boolean existsByBranchCode(String branchCode);
+
+	@Query(value = "select nextval('branch_code_seq')", nativeQuery = true)
+	Long getNextBranchCodeSequence();
 }
